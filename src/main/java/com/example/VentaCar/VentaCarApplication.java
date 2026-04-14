c42015e0edf42b15bac31748ee32618c65057e4c
Benja-Farias <benj.fariasg@duocuc.cl>
2026-04-14 13:52:11 -0400

    Agregar Spring

---
+++ src/main/java/com/example/VentaCar/VentaCarApplication.java
@@ -0,0 +1,13 @@
+package com.example.VentaCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentaCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentaCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VentaCar/VentaCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VentaCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentaCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
