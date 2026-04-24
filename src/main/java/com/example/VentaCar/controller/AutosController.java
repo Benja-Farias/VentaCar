9286f03e1fc74d70abf64ef04581c2d7efff8e99
carlossepulvedadiaz <cssepulvedadiaz@gmail.com>
2026-04-24 14:07:40 -0400

    Repository, services

--- src/main/java/com/example/VentaCar/controller/AutosController.java
+++ src/main/java/com/example/VentaCar/controller/AutosController.java
@@ -1,1 +1,24 @@
+package com.example.VentaCar.controller;
 
+import com.example.VentaCar.model.Autos;
+import com.example.VentaCar.service.AutosService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/autos")
+public class AutosController {
+
+    @Autowired
+    private AutosService autosService;
+
+    @GetMapping
+    public ResponseEntity<List<Autos>> listarAutos() {
+        return ResponseEntity.ok(autosService.getAutos());
+    }
+}
--- src/main/java/com/example/VentaCar/service/AutosService.java
+++ src/main/java/com/example/VentaCar/service/AutosService.java
@@ -1,5 +1,19 @@
 package com.example.VentaCar.service;
 
+import com.example.VentaCar.model.Autos;
+import com.example.VentaCar.repository.AutosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AutosService {
-    
+
+    @Autowired
+    private AutosRepository autosRepository;
+
+    public List<Autos> getAutos() {
+        return autosRepository.findAll();
+    }
 }
