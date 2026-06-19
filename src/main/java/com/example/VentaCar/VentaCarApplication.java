0cf539725720525c80d4966a77ca3d50cdda8110
carlossepulvedadiaz <cssepulvedadiaz@gmail.com>
2026-06-19 14:28:46 -0400

    Swagger token

--- src/main/java/com/example/VentaCar/VentaCarApplication.java
+++ src/main/java/com/example/VentaCar/VentaCarApplication.java
@@ -2,12 +2,30 @@ package com.example.VentaCar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @SpringBootApplication
 public class VentaCarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VentaCarApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VentaCarApplication.class, args);
+    }
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                // Aplica el esquema de seguridad de forma global a todos los endpoints
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                // Define el esquema de seguridad (JWT Bearer Token)
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .name("BearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
 }
